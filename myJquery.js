87ad1e294399a62fa923ca33409a66e85b8e8867
Muhammad Fahad <fahadsahib786@gmail.com>
2021-10-30 11:17:42 +0500

    2

---
+++ myJquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      var input = $("#input").value();
+    //   alert("Text :"+ $("#input").value());
+alert(input);
+});
+
+});
+  
